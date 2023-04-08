4de44897180ec523d01c557af76ec2a0e069268b
츄차원의세계 <lazpberry1012@gmail.com>
2023-04-08 22:02:16 +0900

    Introduction to Express

--- wetube/src/server.js
+++ wetube/src/server.js
@@ -4,6 +4,16 @@ const PORT = 4000;
 
 const app = express();
 
+app.get('/', (req, res) => {
+    console.log("Hey!");
+    return res.send("<h1>Are you surprised?</h1>");
+});
+
+const handleLogin = (req, res) => {
+    return res.send("Login here.");
+}
+app.get("/login", handleLogin)
+
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
