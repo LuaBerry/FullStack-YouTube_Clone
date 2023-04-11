f5cc5f500b27c3198a3aa0731b60e0f685b9ba3b
츄차원의세계 <lazpberry1012@gmail.com>
2023-04-11 23:44:13 +0900

    Middlewares introduction

--- wetube/src/server.js
+++ wetube/src/server.js
@@ -4,15 +4,20 @@ const PORT = 4000;
 
 const app = express();
 
+const middleWare = (req, res, next) => {
+    console.log(`Someone is going to: ${req.url}`);
+    next();
+}
+
 app.get('/', (req, res) => {
     console.log("Hey!");
     return res.send("<h1>Are you surprised?</h1>");
 });
 
-const handleLogin = (req, res) => {
+const handleLogin = (req, res, next) => {
     return res.send("Login here.");
 }
-app.get("/login", handleLogin)
+app.get("/login", middleWare, handleLogin)
 
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
 
