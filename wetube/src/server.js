beaf39f7c0bc99ce917a65be06b816b2e9f96958
츄차원의세계 <lazpberry1012@gmail.com>
2023-04-12 23:09:14 +0900

    Middleware 2

--- wetube/src/server.js
+++ wetube/src/server.js
@@ -4,21 +4,40 @@ const PORT = 4000;
 
 const app = express();
 
-const middleWare = (req, res, next) => {
-    console.log(`Someone is going to: ${req.url}`);
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
     next();
 }
 
-app.get('/', (req, res) => {
-    console.log("Hey!");
-    return res.send("<h1>Are you surprised?</h1>");
-});
+const protectionMiddleware = (req, res, next) => {
+    const url = req.url;
+    if(url === "/protected") {
+        return res.send("<h1>Access denied</h1>");
+    }
+    next();
+}
 
-const handleLogin = (req, res, next) => {
+const handleRoot = (req, res) => {
+    return res.send("<h1>Are you surprised?</h1>");
+}
+
+const handleLogin = (req, res) => {
     return res.send("Login here.");
 }
-app.get("/login", middleWare, handleLogin)
+
+const handleProtected = (req, res) => {
+    return res.send("Welcome to private page.");
+}
+
+app.use(logger);
+app.use(protectionMiddleware);
+
+app.get("/", handleRoot);
+app.get("/login", handleLogin);
+app.get("/protected", handleProtected);
 
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
+
+
 
 app.listen(PORT, handleListening);
