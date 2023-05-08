38ffb4880ec6e65f58aebf3a208b9bcb625d861b
Baskin Lazpberry <lazpberry1012@gmail.com>
2023-05-08 21:36:30 +0900

    Video player Time indicater & Timeline

--- wetube/src/client/js/videoPlayer.js
+++ wetube/src/client/js/videoPlayer.js
@@ -1,10 +1,12 @@
+const { format } = require("morgan");
+
 const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
-const time = document.getElementById("time");
 const volumeRange = document.getElementById("volume");
-
-
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
+const timeline = document.getElementById("timeline");
 
 let volumeValue = 0.5;
 video.volume = volumeValue;
@@ -38,6 +40,33 @@ const handleVolumeChange = (event) => {
     volumeValue = event.target.value;
 }
 
+const formatTime = (seconds) => {
+    const formattedTime = new Date(seconds * 1000).toISOString();
+    if(seconds < 60 * 60) {
+        return formattedTime.substring(14, 19);
+    } else {
+        return formattedTime.substring(11, 19);
+    }
+}
+const handleLoadedMetadata = (event) => {
+    totalTime.innerText = formatTime(Math.floor(video.duration));
+    timeline.max = video.duration;
+}
+
+const handleTimeUpdate = (event) => {
+    currentTime.innerText = formatTime(Math.floor(video.currentTime));
+    timeline.value = Math.floor(video.currentTime);
+}
+
+const handleTimeline = (event) => {
+    video.currentTime = event.target.value;
+}
 playBtn.addEventListener("click", handlePlay);
 muteBtn.addEventListener("click", handleMute);
-volumeRange.addEventListener("input", handleVolumeChange)
+volumeRange.addEventListener("input", handleVolumeChange);
+video.addEventListener("timeupdate", handleTimeUpdate);
+timeline.addEventListener("input", handleTimeline);
+
+video.readyState
+    ? handleLoadedMetadata()
+    : video.addEventListener("loadedmetadata", handleLoadedMetadata);
